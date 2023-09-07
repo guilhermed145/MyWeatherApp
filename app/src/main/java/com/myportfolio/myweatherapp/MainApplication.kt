30aa21fc56651ea9e8dbca4373040d6a79fd741d
guilhermed145 <guilhermed1506@gmail.com>
2023-09-07 11:05:45 -0300

    Create an Application class for the app

---
+++ app/src/main/java/com/myportfolio/myweatherapp/MainApplication.kt
@@ -0,0 +1,14 @@
+package com.myportfolio.myweatherapp
+
+import android.app.Application
+import com.myportfolio.myweatherapp.data.AppContainer
+import com.myportfolio.myweatherapp.data.DefaultAppContainer
+
+class MainApplication: Application() {
+    /** AppContainer instance used by the rest of classes to obtain dependencies */
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultAppContainer()
+    }
+}
