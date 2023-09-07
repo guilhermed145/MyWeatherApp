23d48d1da2728f36f62c9ebf34c120c950d6cd1e
guilhermed145 <guilhermed1506@gmail.com>
2023-09-07 11:08:58 -0300

    Update the ViewModel and the UIState

--- app/src/main/java/com/myportfolio/myweatherapp/ui/AppUiState.kt
+++ app/src/main/java/com/myportfolio/myweatherapp/ui/AppUiState.kt
@@ -1,13 +1,69 @@
 package com.myportfolio.myweatherapp.ui
 
+import com.myportfolio.myweatherapp.domain.model.Condition
+import com.myportfolio.myweatherapp.domain.model.ForecastInfo
+import com.myportfolio.myweatherapp.domain.model.Location
+import com.myportfolio.myweatherapp.domain.model.WeatherInfo
+
 
 /**
  * App UI State
  */
 data class AppUiState(
     val currentScreen: String = "main",
+
+    val weatherInfo: WeatherInfo =
+        WeatherInfo(
+            cloud = 0,
+            condition = Condition(
+                icon = "",
+                text = ""
+            ),
+            is_day = 0,
+            precip_mm = 0.0,
+            temp_c = 0.0,
+            temp_f = 0.0,
+            wind_dir = "",
+            wind_kph = 0.0,
+        ),
+    val currentLocation: Location =
+        Location(
+            country = "",
+            lat = 0.0,
+            localtime = "",
+            lon = 0.0,
+            name = "",
+            region = "",
+            tz_id = ""
+        ),
+    val forecastDayList: List<ForecastInfo> = mutableListOf(
+        ForecastInfo(
+            condition = Condition(
+                icon = "",
+                text = ""
+            ),
+            date = "",
+            daily_chance_of_rain = 0,
+            daily_chance_of_snow = 0,
+            maxtemp_c = 0.0,
+            maxtemp_f = 0.0,
+            maxwind_kph = 0.0,
+            mintemp_c = 0.0,
+            mintemp_f = 0.0
+        )
+    ),
+    val locationSearchResultList: List<Location> = listOf(
+        Location(
+            country = "",
+            lat = 0.0,
+            localtime = "",
+            lon = 0.0,
+            name = "",
+            region = "",
+            tz_id = ""
+        ),
+    ),
     val searchBarText: String = "",
-    val isSearchBarActive: Boolean = false,
-    val searchBarHistory: List<String> = listOf(),
-    val searchResultList: List<String> = listOf(),
+    val showSearchHistory: Boolean = false,
+    val locationHistoryList: List<Location> = listOf(),
 )
--- app/src/main/java/com/myportfolio/myweatherapp/ui/AppViewModel.kt
+++ app/src/main/java/com/myportfolio/myweatherapp/ui/AppViewModel.kt
@@ -1,17 +1,37 @@
 package com.myportfolio.myweatherapp.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.myportfolio.myweatherapp.MainApplication
+import com.myportfolio.myweatherapp.data.AppRepository
+import com.myportfolio.myweatherapp.data.toForecastInfo
+import com.myportfolio.myweatherapp.data.toLocation
+import com.myportfolio.myweatherapp.data.toLocaton
+import com.myportfolio.myweatherapp.data.toWeatherInfo
+import com.myportfolio.myweatherapp.domain.model.ForecastInfo
+import com.myportfolio.myweatherapp.domain.model.Location
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 /**
  * The ViewModel Class
  */
-class AppViewModel : ViewModel(){
+class AppViewModel(private val appRepository: AppRepository) : ViewModel() {
 
     private val _uiState = MutableStateFlow(AppUiState())
     val uiState = _uiState.asStateFlow()
+
+    init {
+        getWeatherInfo("-23.53, -46.62")
+    }
 
     // Tells the app to change the screen
     fun changeScreen(nextScreen : String){
@@ -20,4 +40,84 @@ class AppViewModel : ViewModel(){
         }
     }
 
+    fun updateSearchBarText(text: String = "") {
+        if (text == "") {
+            if (!uiState.value.showSearchHistory) {
+                _uiState.update {
+                    it.copy(showSearchHistory = true)
+                }
+            }
+        }
+        _uiState.update {
+            it.copy(searchBarText = text)
+        }
+    }
+
+    fun addToLocationHistoryList(location: Location) {
+        val newHistory: MutableList<Location> = mutableListOf()
+        newHistory.addAll(uiState.value.locationHistoryList)
+        newHistory.add(location)
+        _uiState.update {
+            it.copy(locationHistoryList = newHistory)
+        }
+    }
+
+    fun getWeatherInfo(cityLocation: String) {
+        viewModelScope.launch {
+            try {
+                val apiData = appRepository.getWeatherInfo(cityLocation)
+                val forecastdayList = mutableListOf<ForecastInfo>()
+                for (day in apiData.forecast.forecastday) {
+                    forecastdayList.add(day.toForecastInfo())
+                }
+                _uiState.update {
+                    it.copy(
+                        weatherInfo = apiData.current.toWeatherInfo(),
+                        currentLocation = apiData.location.toLocation(),
+                        forecastDayList = forecastdayList,
+                    )
+                }
+            } catch (e: IOException) {
+                Log.e("IOEXCEPTION", "getWeatherInfo()", e)
+            } catch (e: HttpException) {
+                Log.e("HTTPEXCEPTION", "getWeatherInfo()", e)
+            }
+        }
+    }
+
+    fun getLocationSearchResults() {
+        viewModelScope.launch {
+            try {
+                val apiData = appRepository.getLocationSearchResults(uiState.value.searchBarText)
+                val foundLocationList = mutableListOf<Location>()
+                for (locationItem in apiData) {
+                    foundLocationList.add(locationItem.toLocaton())
+                }
+                _uiState.update {
+                    it.copy(
+                        locationSearchResultList = foundLocationList,
+                        showSearchHistory = false
+                    )
+                }
+            } catch (e: IOException) {
+                Log.e("API", "getLocationSearchResults()", e)
+            } catch (e: HttpException) {
+                Log.e("HTTPEXCEPTION", "getLocationSearchResults()", e)
+            }
+        }
+    }
+
+    /**
+     * Factory for the viewModel that takes the repository as a dependency
+     */
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+                    as MainApplication)
+                val appRepository = application.container.appRepository
+                AppViewModel(appRepository = appRepository)
+            }
+        }
+    }
 }
