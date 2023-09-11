3320ba2efc6634887aea980aafcd7afcf5ef3544
guilhermed145 <guilhermed1506@gmail.com>
2023-09-11 16:17:23 -0300

    Change the colors for non dynamic Themes

--- app/src/main/java/com/myportfolio/myweatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/myportfolio/myweatherapp/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.myportfolio.myweatherapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val midBlue1 = Color(0xFF3f4761)
+val darkBlue = Color(0xFF10131c)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val midBlue2 = Color(0xFF2a357d)
+val lightGrey1 = Color(0xFFfafcff)
+val lightGrey2 = Color(0xFFebebeb)
+val lightGrey3  = Color(0xFFf2f2f2)
